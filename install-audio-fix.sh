ba0a94000c5bd802222058c8f1251a9845b136ec
jlobue10 <jlobue10@gmail.com>
2023-07-29 19:40:26 -0700

    Add files via upload

---
+++ install-audio-fix.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# A simple script to create the acpi override, install it and necessary files
+# and update the Cirrus firmware files. After running this script and rebooting
+# with a properly patched kernel, audio will be fixed for you on the ROG ALLY
+# running Nobara.
+
+CURRENT_WD=$(pwd)
+iasl -tc cirrus_sddt_patch.dsl
+mkdir -p kernel/firmware/acpi
+cp cirrus_sddt_patch.aml kernel/firmware/acpi
+find kernel | cpio -H newc --create > patched_cirrus_acpi.cpio
+sudo cp patched_cirrus_acpi.cpio /boot/patched_cirrus_acpi.cpio
+sudo cp /etc/default/grub /etc/default/grub-bkp
+sudo cp $CURRENT_WD/grub /etc/default/grub
+sudo cp $CURRENT_WD/50-alsa-config.lua /usr/share/wireplumber/main.lua.d/50-alsa-config.lua
+tar czvf cirrus.tar.gz
+sudo cp -rf $CURRENT_WD/cirrus/ /lib/firmware/
+
+ANS=$?
+if [[ $ANS == 0 ]]; then
+    echo -e "\nROG ALLY Audio fix and audio firmware has been successfully installed.\n"
+else
+    echo -e "\nAn error has occurred. Refer to command line output for details.\n"
+    exit 1
+fi
