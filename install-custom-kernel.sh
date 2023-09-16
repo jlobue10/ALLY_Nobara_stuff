9e90e2df3ca452d8f4ff3925195c78ebc683c9ed
jlobue10 <jlobue10@gmail.com>
2023-09-16 11:13:21 -0700

    Update install-custom-kernel.sh

--- install-custom-kernel.sh
+++ install-custom-kernel.sh
@@ -18,36 +18,8 @@ dnf install fedpkg fedora-packager rpmdevtools rpmlint ncurses-devel pesign grub
 wget https://download.copr.fedorainfracloud.org/results/gloriouseggroll/nobara/fedora-38-x86_64/06403856-kernel/kernel-6.5.3-200.fsync.fc38.src.rpm
 rm -rf $HOME/rpmbuild/
 rpm -Uvh kernel-6.5.3-200.fsync.fc38.src.rpm
-#cp $CURRENT_WD/kernel.spec $HOME/rpmbuild/SPECS
 rpmbuild -bp $HOME/rpmbuild/SPECS/kernel.spec
-cp $CURRENT_WD/{asus-rog-ally-side-buttons.patch,OpenRGB-patch-fix.patch,modpost.patch} $HOME/rpmbuild/SOURCES/
 cd $HOME/rpmbuild/SOURCES
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.3.tar.xz
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/patch-6.5-redhat.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0001-platform-x86-asus-wmi-add-support-for-showing-charge.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0002-platform-x86-asus-wmi-add-support-for-showing-middle.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0003-platform-x86-asus-wmi-support-middle-fan-custom-curv.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0004-platform-x86-asus-wmi-add-WMI-method-to-show-if-egpu.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0005-platform-x86-asus-wmi-don-t-allow-eGPU-switching-if-.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0006-platform-x86-asus-wmi-add-safety-checks-to-gpu-switc.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0007-platform-x86-asus-wmi-support-setting-mini-LED-mode.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0008-platform-x86-asus-wmi-expose-dGPU-and-CPU-tunables-f.patch
-wget https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.5/0009-Fixes-a23870110a38-asus-wmi-add-support-for-showing-.patch
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/v2-0001-ALSA-hda-cs35l41-Support-systems-with-missing-_DS.patch
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/v2-0002-ALSA-hda-cs35l41-Support-ASUS-2023-laptops-with-m.patch
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/v2-0001-platform-x86-asus-wmi-corrections-to-egpu-safety-.patch
-wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/v6-0001-platform-x86-asus-wmi-add-support-for-ASUS-screen.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0001-ALSA-cs35l41-Use-mbox-command-to-enable-speaker-outp.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0002-ALSA-cs35l41-Poll-for-Power-Up-Down-rather-than-wait.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0003-ALSA-hda-cs35l41-Check-mailbox-status-of-pause-comma.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0004-ALSA-hda-cs35l41-Ensure-we-correctly-re-sync-regmap-.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0005-ALSA-hda-cs35l41-Ensure-we-pass-up-any-errors-during.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0006-ALSA-hda-cs35l41-Move-Play-and-Pause-into-separate-f.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0007-ALSA-hda-hda_component-Add-pre-and-post-playback-hoo.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0008-ALSA-hda-cs35l41-Use-pre-and-post-playback-hooks.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0009-ALSA-hda-cs35l41-Rework-System-Suspend-to-ensure-cor.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0010-ALSA-hda-cs35l41-Add-device_link-between-HDA-and-cs3.patch
-wget https://raw.githubusercontent.com/ChimeraOS/linux-chimeraos/main/linux/0011-ALSA-hda-cs35l41-Ensure-amp-is-only-unmuted-during-p.patch
 wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/0001-ALSA-cs35l41-Handle-mdsync_down-reg-write-errors.patch
 wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/0002-ALSA-cs35l41-Handle-mdsync_up.patch
 wget https://github.com/jlobue10/ALLY_Nobara_fixes/raw/main/0003-ALSA-cs35l41-Initialize-completion-object-before-requesting-IRQ.patch
