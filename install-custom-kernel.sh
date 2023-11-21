2bea71325a0f4c12a7c6b8aae595c5d329ef9193
jlobue10 <jlobue10@gmail.com>
2023-11-21 07:45:46 -0800

    Update install-custom-kernel.sh

--- install-custom-kernel.sh
+++ install-custom-kernel.sh
@@ -46,10 +46,11 @@ wget https://github.com/NeroReflex/linux-chimeraos/blob/v6.6/linux/uinput.patch
 # end NeroReflex pacth additions
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.2.tar.xz
 cp $CURRENT_WD/kernel.spec $HOME/rpmbuild/SPECS
-rpmbuild -bp $HOME/rpmbuild/SPECS/kernel.spec
+rpmbuild -bp $HOME/rpmbuild/SPECS/kernel.spec > $HOME/kernel-patch-rpm-prep-log.txt
 cp $CURRENT_WD/{.config,Makefile} $HOME/rpmbuild/BUILD/kernel-6.6.2/linux-6.6.2-200.fsync.ally.fc38.x86_64/
 cd $HOME/rpmbuild/BUILD/kernel-6.6.2/linux-6.6.2-200.fsync.ally.fc38.x86_64/
-time make bzImage -j8 && make modules -j8
+time make bzImage -j8 > $HOME/make-log.txt
+make modules -j8 > $HOME/make-modules-log.txt
 make modules_install -j8
 make install
 
