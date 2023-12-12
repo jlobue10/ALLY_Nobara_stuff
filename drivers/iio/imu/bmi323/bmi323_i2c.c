18647b167ee3bf2cec2fd1717306aa821ac9e53e
jlobue10 <jlobue10@gmail.com>
2023-12-12 07:12:46 -0800

    Update bmi323_i2c.c
    Patch fix-ups

--- drivers/iio/imu/bmi323/bmi323_i2c.c
+++ drivers/iio/imu/bmi323/bmi323_i2c.c
@@ -12,6 +12,11 @@
 
 #include "bmi323.h"
 
+struct bmi323_i2c_priv {
+	struct i2c_client *i2c;
+	u8 i2c_rx_buffer[BMI323_FIFO_LENGTH_IN_BYTES + BMI323_I2C_DUMMY];
+};
+
 /*
  * From BMI323 datasheet section 4: Notes on the Serial Interface Support.
  * Each I2C register read operation requires to read two dummy bytes before
@@ -21,34 +26,25 @@ static int bmi323_regmap_i2c_read(void *context, const void *reg_buf,
 				  size_t reg_size, void *val_buf,
 				  size_t val_size)
 {
-	struct device *dev = context;
-	struct i2c_client *i2c = to_i2c_client(dev);
+	struct bmi323_i2c_priv *priv = context;
 	struct i2c_msg msgs[2];
-	u8 *buff = NULL;
 	int ret;
 
-	buff = kmalloc(val_size + BMI323_I2C_DUMMY, GFP_KERNEL);
-	if (!buff)
-		return -ENOMEM;
-
-	msgs[0].addr = i2c->addr;
-	msgs[0].flags = i2c->flags;
+	msgs[0].addr = priv->i2c->addr;
+	msgs[0].flags = priv->i2c->flags;
 	msgs[0].len = reg_size;
 	msgs[0].buf = (u8 *)reg_buf;
 
-	msgs[1].addr = i2c->addr;
+	msgs[1].addr = priv->i2c->addr;
 	msgs[1].len = val_size + BMI323_I2C_DUMMY;
-	msgs[1].buf = (u8 *)buff;
-	msgs[1].flags = i2c->flags | I2C_M_RD;
+	msgs[1].buf = priv->i2c_rx_buffer;
+	msgs[1].flags = priv->i2c->flags | I2C_M_RD;
 
-	ret = i2c_transfer(i2c->adapter, msgs, ARRAY_SIZE(msgs));
-	if (ret < 0) {
-		kfree(buff);
+	ret = i2c_transfer(priv->i2c->adapter, msgs, ARRAY_SIZE(msgs));
+	if (ret < 0)
 		return -EIO;
-	}
 
-	memcpy(val_buf, buff + BMI323_I2C_DUMMY, val_size);
-	kfree(buff);
+	memcpy(val_buf, priv->i2c_rx_buffer + BMI323_I2C_DUMMY, val_size);
 
 	return 0;
 }
@@ -56,16 +52,13 @@ static int bmi323_regmap_i2c_read(void *context, const void *reg_buf,
 static int bmi323_regmap_i2c_write(void *context, const void *data,
 				   size_t count)
 {
-	struct device *dev = context;
-	struct i2c_client *i2c = to_i2c_client(dev);
-	int ret;
+	struct bmi323_i2c_priv *priv = context;
 	u8 reg;
 
 	reg = *(u8 *)data;
-	ret = i2c_smbus_write_i2c_block_data(i2c, reg, count - 1,
-					     data + sizeof(u8));
-
-	return ret;
+	return i2c_smbus_write_i2c_block_data(priv->i2c, reg,
+					      count - sizeof(u8),
+					      data + sizeof(u8));
 }
 
 static struct regmap_bus bmi323_regmap_bus = {
@@ -73,13 +66,26 @@ static struct regmap_bus bmi323_regmap_bus = {
 	.write = bmi323_regmap_i2c_write,
 };
 
+const struct regmap_config bmi323_i2c_regmap_config = {
+	.reg_bits = 8,
+	.val_bits = 16,
+	.max_register = BMI323_CFG_RES_REG,
+	.val_format_endian = REGMAP_ENDIAN_LITTLE,
+};
+
 static int bmi323_i2c_probe(struct i2c_client *i2c)
 {
 	struct device *dev = &i2c->dev;
+	struct bmi323_i2c_priv *priv;
 	struct regmap *regmap;
 
-	regmap = devm_regmap_init(dev, &bmi323_regmap_bus, dev,
-				  &bmi323_regmap_config);
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->i2c = i2c;
+	regmap = devm_regmap_init(dev, &bmi323_regmap_bus, priv,
+				  &bmi323_i2c_regmap_config);
 	if (IS_ERR(regmap))
 		return dev_err_probe(dev, PTR_ERR(regmap),
 				     "Failed to initialize I2C Regmap\n");
@@ -88,7 +94,7 @@ static int bmi323_i2c_probe(struct i2c_client *i2c)
 }
 
 static const struct i2c_device_id bmi323_i2c_ids[] = {
-	{ "bmi323", 0 },
+	{ "bmi323" },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, bmi323_i2c_ids);
@@ -104,7 +110,7 @@ static struct i2c_driver bmi323_i2c_driver = {
 		.name = "bmi323",
 		.of_match_table = bmi323_of_i2c_match,
 	},
-	.probe_new = bmi323_i2c_probe,
+	.probe = bmi323_i2c_probe,
 	.id_table = bmi323_i2c_ids,
 };
 module_i2c_driver(bmi323_i2c_driver);
